43dd47814a4a45856bcb6a9b171021829977e6d0
Marcelo Candia <candia.munoz.marcelo@gmail.com>
2023-05-18 14:32:27 -0400

    Splash Screen Implementation

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,12 +6,14 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
 import 'package:location_web/location_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FlutterNativeSplashWeb.registerWith(registrar);
   LocationWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
