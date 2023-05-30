46d9a9fa7e52454bcc3f7303d84323c79ffd5ca6
Marcelo Candia <candia.munoz.marcelo@gmail.com>
2023-05-30 16:15:33 -0400

    hotfix

--- lib/pages/homepage/homepage.dart
+++ lib/pages/homepage/homepage.dart
@@ -52,7 +52,8 @@ class _HomePageState extends State<HomePage> {
           } else {
             return (selectedIndex == 0)
 /* ======================================= WITH REFRESH INDICATOR ======================================= */
-            ?RefreshIndicator(
+            ?
+            RefreshIndicator(
               backgroundColor: const Color(0xFFBDEFFF),
               color: const Color(0xFF0071E4),
               triggerMode: RefreshIndicatorTriggerMode.onEdge,
@@ -247,66 +248,6 @@ class _HomePageState extends State<HomePage> {
                     /* -------------------- END OPTIONS BOTTOM NAVIGATION BAR ITEM -------------------- */
                   ]
                 ),
-            
-                /* ==================== FLOATING ACTION BUTTON ==================== */
-                floatingActionButton: (selectedIndex == 0)
-                ? StreamBuilder<bool>(
-                  stream: FlutterBluePlus.instance.isScanning,
-                  initialData: false,
-                  builder: (c, snapshot) {
-                    if (snapshot.data!) {
-                      return SizedBox(
-                        height: height * 0.05,
-                        width:  width * 0.5,
-                        child: FloatingActionButton(
-                          shape: const StadiumBorder(
-                            side: BorderSide(
-                              color: Color(0xFF00C0FF)
-                            )
-                          ),
-                          isExtended: true,
-                          onPressed: () {
-                            FlutterBluePlus.instance.stopScan();
-                          },
-                          backgroundColor: const Color(0xFFBDEFFF),
-                          foregroundColor: Colors.black,
-                          child: Text(
-                            "Detener búsqueda",
-                            style: TextStyle(
-                              fontSize: hxw * 0.000045,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                      );
-                    } else {
-                      return FloatingActionButton( /* VER LOCALIZACION DEL FAB */
-                        backgroundColor: const Color(0xFF00C0FF),
-                          child: const Icon(Icons.search, color: Colors.white,),
-                          onPressed: () async {
-                            if (await Permission.location.isGranted) {
-                              Location location = Location();
-                              bool isOn = await location.serviceEnabled(); 
-                              if (!isOn) { //if defvice is off
-                                bool isturnedon = await location.requestService();
-                                if (isturnedon) {
-                                  /* print("GPS device is turned ON"); */
-                                }else{
-                                  /* print("GPS Device is still OFF"); */
-                                }
-                              } else {
-                                refreshIndicatorKey.currentState?.show();
-                              }
-                            } else {
-                              locationPermissionDialog();
-                            }
-                          }
-                      );
-                    }
-                  }
-                )
-                : null
-                /* ==================== END FLOATING ACTION BUTTON ==================== */
               );
 /* ======================================= END WITHOUT REFRESH INDICATOR ======================================= */
           }
@@ -362,13 +303,5 @@ class _HomePageState extends State<HomePage> {
     ).then((value) => false);
   }
   /* ==================== END LOCATION PERMISSION DIALOG ==================== */
-
-  /* ==================== REFRESH ==================== */
-  refresh(){
-    if (selectedIndex == 0) {
-      
-    }
-  }
-  /* ==================== END REFRESH ==================== */
 }
   
